0251f17f30df30a62d7dc103d71648a12ee97b9b
cygday <amethystpun98@gmail.com>
2025-04-30 16:37:36 +0545

    Update App.java

--- simple-java-project/src/main/java/com/example/App.java
+++ simple-java-project/src/main/java/com/example/App.java
@@ -1,6 +1,6 @@
-public class App{
+public class App {
 	public static void main(String[] args)
 {
-		System.out.printIn("hello, sonarqube + jenkins!");
+		System.out.println("hello, sonarqube + jenkins!");
 	}
 }
